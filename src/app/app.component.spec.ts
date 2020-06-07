5cbc30d8c4f97c6ccddd82933568c09f34dceb4b
vivianlsong <63174230+vivianlsong@users.noreply.github.com>
2020-06-07 15:06:48 -0700

    updated test files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('grocery-list app is running!');
+    expect(compiled.querySelector('title').textContent).toContain('grocery-list');
   });
 });
